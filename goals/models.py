405112fbbbbca0cd60ea7822591c714a59f1fc49
UsefulSan <usefulsan@yandex.ru>
2023-03-14 12:18:36 +0300

    migrations for the board have been applied

--- goals/models.py
+++ goals/models.py
@@ -48,8 +48,7 @@ class GoalCategory(DatesModelMixin):
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
-    board = models.ForeignKey(Board, verbose_name="Доска", on_delete=models.PROTECT,
-                              related_name="categories", null=True)
+    board = models.ForeignKey(Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="categories")
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
